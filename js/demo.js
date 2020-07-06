f24c036c73f1c7d4770dbd4aeb785e1ae9f35441
Minh Bui <tranduyminh.bui@gmail.com>
2020-07-06 21:27:42 +1000

    fixed smooth animation between landing page and project page

--- js/demo.js
+++ js/demo.js
@@ -305,3 +305,99 @@
     }
   );
 }
+
+const fadeOut = () => {
+  // Landing page
+  TweenMax.to('.myBtn', 0.5, {
+    y: -100,
+    opacity: 0,
+  });
+
+  TweenMax.to('.screen', 0.5, {
+    y: -400,
+    opacity: 0,
+    ease: Power2.easeInOut,
+    delay: 1,
+  });
+
+  TweenMax.from('.overlay', 1, {
+    ease: Power2.easeInOut,
+  });
+
+  TweenMax.to('.overlay', 1, {
+    delay: 1,
+    top: '-110%',
+    ease: Expo.easeInOut,
+  });
+
+  // Flashing screen
+  TweenMax.to('.overlay-2', 0.5, {
+    delay: 1.5,
+    top: '-110%',
+    ease: Expo.easeInOut,
+  });
+
+  // Main gallery
+  TweenMax.from('.content', 1, {
+    delay: 1.8,
+    opacity: 0,
+    y: '100%',
+    ease: Power2.easeInOut,
+  });
+
+  TweenMax.to('.content', 1, {
+    opacity: 1,
+    y: 0,
+    delay: 2,
+    ease: Power2.easeInOut,
+  });
+};
+
+const fadeIn = () => {
+  // Main gallery
+  TweenMax.to('.content', 0.5, {
+    opacity: 0,
+    ease: Power2.easeInOut,
+  });
+  TweenMax.to('.content', 0.5, {
+    y: '0%',
+    delay: 0.5,
+  });
+  // TweenMax.to('.overlay-2', 1, {
+  //   delay: 0.5,
+  //   top: '110%',
+  //   ease: Expo.easeInOut,
+  // });
+  TweenMax.to('.overlay-2', 0.5, {
+    delay: 0.5,
+    top: '0%',
+    ease: Expo.easeInOut,
+  });
+
+  // TweenMax.from('.overlay', 1, {
+  //     delay: 2,
+  //     opacity: 0,
+  //     ease: Power2.easeInOut,
+  //   });
+
+  TweenMax.to('.overlay', 1, {
+    delay: 0.5,
+    top: '0%',
+    opacity: 1,
+    ease: Expo.easeInOut,
+  });
+
+  TweenMax.to('.screen', 0.5, {
+    y: 0,
+    opacity: 1,
+    delay: 1,
+    ease: Power2.easeInOut,
+  });
+
+  TweenMax.to('.myBtn', 0.5, {
+    y: 0,
+    // top: '50%',
+    // left: '50%',
+    opacity: 1,
+  });
+};
