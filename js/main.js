b84b7c326a1ee12d4e137d3a46199a8f8940a4e1
Minh Bui <tranduyminh.bui@gmail.com>
2020-07-13 12:01:30 +1000

    testing video autoplay on iphone

--- js/main.js
+++ js/main.js
@@ -122,29 +122,29 @@ menu.onclick = () => {
 // });
 
 // Video
-function playVisibleVideos() {
-  document
-    .querySelectorAll('video')
-    .forEach((video) =>
-      elementIsVisible(video) ? video.play() : video.pause()
-    );
-}
+// function playVisibleVideos() {
+//   document
+//     .querySelectorAll('video')
+//     .forEach((video) =>
+//       elementIsVisible(video) ? video.play() : video.pause()
+//     );
+// }
 
-function elementIsVisible(el) {
-  let rect = el.getBoundingClientRect();
-  return (
-    rect.bottom >= 0 &&
-    rect.right >= 0 &&
-    rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.left <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
+// function elementIsVisible(el) {
+//   let rect = el.getBoundingClientRect();
+//   return (
+//     rect.bottom >= 0 &&
+//     rect.right >= 0 &&
+//     rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+//     rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+//   );
+// }
 
-let playVisibleVideosTimeout;
-window.addEventListener('scroll', () => {
-  clearTimeout(playVisibleVideosTimeout);
-  playVisibleVideosTimeout = setTimeout(playVisibleVideos, 100);
-});
+// let playVisibleVideosTimeout;
+// window.addEventListener('scroll', () => {
+//   clearTimeout(playVisibleVideosTimeout);
+//   playVisibleVideosTimeout = setTimeout(playVisibleVideos, 100);
+// });
 
-window.addEventListener('resize', playVisibleVideos);
-window.addEventListener('DOMContentLoaded', playVisibleVideos);
+// window.addEventListener('resize', playVisibleVideos);
+// window.addEventListener('DOMContentLoaded', playVisibleVideos);
